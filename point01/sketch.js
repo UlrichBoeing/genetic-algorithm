c5b388313b33e2259cdde8db68650d15d88d4e0d
UlrichBoeing <songs.of.letting.go@gmail.com>
2017-06-10 02:13:07 +0200

    separate-files

--- point01/sketch.js
+++ point01/sketch.js
@@ -38,41 +38,7 @@ function mousePressed() {
 }
 
 
-// Sensoren dürfen sich nicht bewegen, shape bewegt sensor
-// erstmal nicht !Sensoren sollen eine gewichtete fitness-history haben (nicht bei Positions-Sensor)
 
-// holt fitness von sensor 
-// berechnet streuung mit fitness
-// berechnet x neue Punkte mit gaussianRandom
-// sucht den besten aus 
-// von von vorne 
-
-// Sensor kann neue Punkte vorschlagen???
-
-
-function Sensor(target) {
-    this.fitness = -1;
-    this.target = target;
-}
-
-Sensor.prototype.calcFitness = function(x, y) {
-    // calculate maxDistance;
-    var maxDistance = createVector();
-    maxDistance.x = 300 + Math.abs(this.target.x - 300);
-    maxDistance.y = 300 + Math.abs(this.target.y - 300);
-
-    
-    var v = createVector(x, y);
-    v.sub(target);
-    this.fitness = 1 - (v.mag() / maxDistance.mag());
-    return this.fitness;
-}
-
-function Shape(x, y) {
-    this.pos = createVector(x, y);
-    this.sensor01 = new Sensor(target);
-
-}
 
 Shape.prototype.move = function() {
     var fitness = this.sensor01.calcFitness(this.pos.x, this.pos.y);
